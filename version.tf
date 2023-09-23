2ba6a0f99e7bae5cdac1ad7831f17f2884b330a5
Doh <kmdoh99@gmail.com>
2023-09-22 23:30:06 -0500

    sns-qa

--- version.tf
+++ version.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     aws = {
         source = "hashicorp/aws"
-        version = "4.61.0"
+        version = "5.5.0"
     }
   }
 }
 provider "aws" {
-    region = "us-east-2"
+    region = "us-east-1"
   
 }
